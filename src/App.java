dcfcc25a6b1cbe7b3fd6dd9650c4865d83d43570
juank <juank@DESKTOP-SB57D4I>
2021-07-24 11:00:51 -0500

    Se agrego una clase a DatosImpresoras y se inicializo el App

--- src/App.java
+++ src/App.java
@@ -1,15 +1,15 @@
 public class App {
     public static void main(String[] args){
-        DatosImpresoras.crearImpresora("L355", "Hewlett-Packard", false);
-        DatosImpresoras.crearImpresora("L3150", "EPSON", true);
-        DatosImpresoras.crearImpresora("E471", "CANON", true);
+        Impresora i1 = new Impresora("L355", "Hewlett-Packard", false);
+        Impresora i2 = new Impresora("L3150", "EPSON", true);
+        Impresora i3 = new Impresora("E471", "CANON", true);
+
+        DatosImpresoras.registrarImpresora(i1);
+        DatosImpresoras.registrarImpresora(i2);
+        DatosImpresoras.registrarImpresora(i3);
 
         DatosImpresoras.buscarImpresora("L355");
 
-        DatosImpresoras.consultarImpresoras();
-        
-
-
-
+        DatosImpresoras.imprimirDatos();
     }
 }
--- src/DatosImpresoras.java
+++ src/DatosImpresoras.java
@@ -1,27 +1,31 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class DatosImpresoras {
 
-    private static List<Impresora> impresoras;
+    private static List<Impresora> impresoras = new ArrayList<>();
 
     public static List<Impresora> consultarImpresoras(){
         return impresoras;
     }
 
+    public static void imprimirDatos(){
+        for (int i = 0; i < DatosImpresoras.consultarImpresoras().size(); i++) {
+            System.out.println("Impresora "+DatosImpresoras.consultarImpresoras().get(i).getMarca()+" con serial "+DatosImpresoras.consultarImpresoras().get(i).getSerial());
+        }
+    }
+
     public static Impresora buscarImpresora(String serial){
         for(Impresora i: impresoras){
             if(i.getSerial() == serial){
+                System.out.println("Impresora: serial "+i.getSerial()+" marca "+i.getMarca());
                 return i;
             }
         }
         return null;
     }
 
-    public static void crearImpresora(String newSerial, String newMarca, Boolean newEscaner){
-        System.out.println(newSerial+"  "+newMarca+" "+newEscaner);
-        Impresora newImpresora = new Impresora(newSerial, newMarca, newEscaner);
-
-        impresoras.add(newImpresora);
-
+    public static void registrarImpresora(Impresora impresora){
+        impresoras.add(impresora);
     }
 }
